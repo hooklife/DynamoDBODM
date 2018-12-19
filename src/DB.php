be4d4a9159485bf6df133c9a53266a4e139d56cf
hooklife <miaogaopeng123@gmail.com>
2018-12-19 16:44:18 +0800

    Add DB default connect param

--- src/DB.php
+++ src/DB.php
@@ -1,19 +1,27 @@
 <?php
-namespace Hoooklife\DynamodbPodm;
-use Hoooklife\DynamodbPodm\Query\Builder;
-class DB {
-    public static $config = [];
 
-    public static function config(array $config)
+namespace Hoooklife\DynamodbPodm;
+
+use Hoooklife\DynamodbPodm\Query\Builder;
+
+class DB
+{
+    public static $config = [];
+    public static $connect;
+
+    public static function config(array $config, string $connect = 'default')
     {
         self::$config = $config;
+        self::$connect = $connect;
+
     }
 
-    public static function table(string $table) {
-        $query = new Builder();
+    public static function table(string $table)
+    {
+        $query = new Builder(self::$connect);
         $query->from($table);
         return $query;
     }
-    
+
 
 }
